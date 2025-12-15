2cca6771740d3ff0aef36ecb423fc6c4c45930a2
Lucas Gabriel W de Souza <lucasgabrielwdesouza@gmail.com>
2025-12-15 19:42:36 -0300

    Update main.js

--- Ramos-Tatto/src/main.js
+++ Ramos-Tatto/src/main.js
@@ -9,3 +9,27 @@ if (scrollIcon) {
     }
   });
 }
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+const logo = document.querySelector(".logo-glow");
+
+logo.addEventListener("mousemove", (e) => {
+  const rect = logo.getBoundingClientRect();
+  const x = ((e.clientX - rect.left) / rect.width) * 100;
+  const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+  logo.style.setProperty("--x", `${x}%`);
+  logo.style.setProperty("--y", `${y}%`);
+});
+
+logo.addEventListener("mouseleave", () => {
+  logo.style.removeProperty("--x");
+  logo.style.removeProperty("--y");
+});
